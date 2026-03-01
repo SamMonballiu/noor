6a60bac1fb70d42e0d1164c66ff00fd44bd8df8a
SamMonballiu <samsonmonballiu@hotmail.com>
2026-03-01 11:39:56 +0100

    backend: add query infrastructure

---
+++ backend/queries/base.ts
@@ -0,0 +1,76 @@
+import { AlbumsQueryHandler } from "../../features/albums/get/getAlbumsQuery";
+import { DataContext } from "../../models/dataContext";
+import { Response } from "express";
+
+export interface Query {}
+
+export interface QueryHandler<T extends Query> {
+  handle: (query: T, context: DataContext) => Promise<QueryResult<any>>;
+  canHandle: (query: T) => boolean;
+}
+
+type QueryResultType = "success" | "error" | "notfound";
+
+export class QueryResult<T> {
+  public type: QueryResultType;
+  public message?: string;
+  public response?: T;
+
+  private constructor(type: QueryResultType, message?: string, response?: T) {
+    this.type = type;
+    this.message = message;
+    this.response = response;
+  }
+
+  public static Success(response: any) {
+    return new QueryResult("success", undefined, response);
+  }
+
+  public static Error(message: string) {
+    return new QueryResult("error", message);
+  }
+
+  public static NotFound() {
+    return new QueryResult("notfound");
+  }
+}
+
+export class QueryResolver {
+  private readonly context: DataContext;
+  private handlers: QueryHandler<any>[] = [new AlbumsQueryHandler()];
+
+  constructor(context: DataContext) {
+    this.context = context;
+  }
+
+  private async resolveQuery(query: Query): Promise<QueryResult<any>> {
+    const handler = this.handlers.find((x) => x.canHandle(query));
+
+    return handler !== undefined
+      ? handler.handle(query, this.context)
+      : QueryResult.Error("Couldn't find a handler for a query of this type");
+  }
+
+  public async resolve(query: Query, res: Response) {
+    const result = await this.resolveQuery(query);
+    handleQueryResult(result, res);
+  }
+}
+
+type QueryResultHandler = (result: QueryResult<any>, res: Response) => void;
+
+const handleQueryResult: QueryResultHandler = (
+  result: QueryResult<any>,
+  res: Response,
+) => {
+  switch (result.type) {
+    case "success":
+      res.status(200).send(result.response);
+      break;
+    case "notfound":
+      res.status(404);
+    case "error":
+      res.status(500).send(result); //TODO make an ErrorResponse or something
+      break;
+  }
+};
