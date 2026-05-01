b33feb86b0afd52541affdee024de9c806a70e98
SamMonballiu <samsonmonballiu@hotmail.com>
2026-05-01 14:59:34 +0200

    crawler: add file size

--- crawler.ts
+++ crawler.ts
@@ -1,4 +1,5 @@
 const { fdir } = require("fdir");
+const crypto = require("crypto");
 import { parseFile } from "music-metadata";
 import fs from "fs";
 import * as dotenv from "dotenv";
@@ -27,30 +28,29 @@ const getTracks = async () => {
     const filePath = filtered[i];
 
     const fullPath = `${folder}/${filePath}`;
-    parseFile(fullPath, {
-      skipCovers: true,
-    }).then((metadata) => {
-      results.push({
-        number: metadata.common.track.no ?? undefined,
-        title: metadata.common.title,
-        album: metadata.common.album,
-        artists: metadata.common.artists,
-        year: metadata.common.year ?? undefined,
-        genre: metadata.common.genre,
-        duration: metadata.format.duration,
-        path: sanitize(fullPath),
-        albumPath: sanitize(fullPath.split("/").slice(0, -1).join("/")),
-      });
 
-      if (results.length % 200 === 0) {
-        console.log(results.length);
-      }
-
-      if (i === filtered.length - 1) {
-        console.log("Writing.");
-        fs.writeFileSync("./tracks.json", JSON.stringify(results));
-      }
+    const metadata = await parseFile(fullPath, { skipCovers: true });
+    results.push({
+      number: metadata.common.track.no ?? undefined,
+      title: metadata.common.title,
+      album: metadata.common.album,
+      artists: metadata.common.artists,
+      year: metadata.common.year ?? undefined,
+      genre: metadata.common.genre,
+      duration: metadata.format.duration,
+      path: sanitize(fullPath),
+      albumPath: sanitize(fullPath.split("/").slice(0, -1).join("/")),
+      size: fs.statSync(fullPath).size,
     });
+
+    if (results.length % 200 === 0) {
+      console.log(results.length);
+    }
+
+    if (i === filtered.length - 1) {
+      console.log("Writing.");
+      fs.writeFileSync("./tracks.json", JSON.stringify(results));
+    }
   }
 };
 
